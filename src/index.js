3b2dab1a1c88ed9db9a946b7e9d2fe209a503edf
arjanmitra <arjanmitra@hotmail.com>
2021-02-18 21:25:17 -0500

    completed acme-users

--- src/index.js
+++ src/index.js
@@ -1,6 +1,43 @@
-//const faker = require('faker');
+const html = require('html-template-tag');
+const faker = require('faker');
 
+let arr = JSON.parse(window.localStorage.getItem('users'));
 
-console.log("here")
+if (!arr) {
+  arr = new Array(50).fill('').map((_) => faker.helpers.userCard());
+  window.localStorage.setItem('users', JSON.stringify(arr));
+}
 
-//const arr = new Array(50).fill('').map(_ => faker.)
+console.log(faker.helpers.userCard());
+
+const userlist = document.querySelector('#user-list');
+
+let curr = window.location.hash.slice(1) * 1;
+
+const render = () => {
+  const str = `
+    ${arr
+      .map(
+        (userObj, i) =>
+          `<a href='#${i}'><li>${userObj.name}</li></a>
+            ${
+              curr === i
+                ? `
+            <ul>
+              ${`<li>${userObj.phone}, ${userObj.address.city}</li>`}
+            </ul>`
+                : ''
+            }
+    `
+      )
+      .join('')}
+  `;
+  userlist.innerHTML = str;
+};
+
+render();
+
+window.addEventListener('hashchange', () => {
+  curr = window.location.hash.slice(1) * 1;
+  render();
+});
